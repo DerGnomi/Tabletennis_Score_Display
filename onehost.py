6b3cbcaa350be81a874ec20e6e64d37fb7b5314f
Dominic Hößl <dominic.hoessl@noris.de>
2019-03-22 09:39:26 +0100

    maybe solved the shifting problem

--- onehost.py
+++ onehost.py
@@ -24,6 +24,9 @@ rightdown=23
 rightup=22
 switch=26
 
+#test the buttons later with
+# pull_up=False
+# and delete "not" in main()
 blu=Button(leftup,pin_factory=local_factory) #button left up
 bld=Button(leftdown,pin_factory=local_factory)  #button left down
 brd=Button(rightdown,pin_factory=local_factory) #button right down
@@ -48,43 +51,53 @@ leds=[61404, 60996, 61304, 61292, 61156, 61356, 61372, 61252, 61436, 61420, 9180
 #Output defs
 
 def shiftout():
-  print (get_sumled())
   led_status_r.on()
+  #status led indicates the shift for debug
+  #set some vars
+  #get the decimal from leds array
   digit = int(leds[get_sumled()])
-  print digit
+  #convert the decimal to binary
+  #strip of the first two binary indicator bits (0b)
   b_digit = bin(digit)[2:]
-  print (b_digit)
-  b_len = len(b_digit) 
-  print (b_len)
+  #read the length of the bitstring
+  b_len = len(b_digit)
+  #set how much bits are missing to get the full 16bit length
+  offset = 16-b_len
+  #debug output
+  #can be delete or commented
+  print (get_sumled())
+  print b_len
+  print digit
+  print b_digit
+  #prepare to shift data into register
   data.off()
-  offset=16-b_len
   latch.on()
   if offset > 0:
     for i in range(offset):
       clocking()
   for bit in b_digit:
+    offset += 1
     if ( int(bit) == 0 ):
       data.off()
     else:
       data.on()
-    clocking()
-  #shifting is one too far
+    if offset != 16:
+      clocking()
+  #shifting is one too far | maybe after the latch is closed
+  #another bit is shifted with the gate opening
   latch.off()
   clocking()
   led_status_r.off()
 
-def reversed( y, num_bits ):
-  x = bin(y)[2:]  
-  answer = 0
-  for i in range( num_bits ):                   # for each bit number
-    if (x & (1 << i)):                        # if it matches that bit
-      answer |= (1 << (num_bits - 1 - i))   # set the "opposite" bit in answer
-  return answer
-
 def set_sumled(su):
+  #debug output
   print ("Set new state")
-  global sumled 
+  global sumled
   sumled += su
+  #set sumled to 0 if its lower than 0 and bigger than 99
+  #to avoid breaking the board
+  if 0 > sumled > 99:
+    sumled = 0
   shiftout()
 
 def get_sumled():
@@ -96,38 +109,49 @@ def clocking():
   clock.off()
   sleep(pauseclock)
 
-#def digital_Write():
-#  latch.off()
-#  clocking()
-#  latch.on()
-
 #main part
 def main():
   running=True
+  error=False
+  error_count=0
   shiftout()
   while running == True:
     try:
-      if bs.is_pressed:
-        if ledstatus.is_lit:
+      while error == False:
+        if bs.is_pressed:
+          if ledstatus.is_lit:
+            ledstatus.off()
+          if not blu.is_pressed:
+            set_sumled(10)
+          if not bld.is_pressed:
+            set_sumled(-10)
+          if not brd.is_pressed:
+            set_sumled(-1)
+          if not bru.is_pressed:
+            set_sumled(1)
+          print("run")
+          sleep(0.5)
+        else:
+          ledstatus.on()
+          print("no")
+          bs.wait_for_press(timeout=None)
+      if not bs.is_pressed:
           ledstatus.off()
-        if not blu.is_pressed:
-          set_sumled(10)
-        if not bld.is_pressed:
-          set_sumled(-10)
-        if  not brd.is_pressed:
-          set_sumled(-1)
-        if not bru.is_pressed:
-          set_sumled(1)
-        print("run")
-        sleep(0.5)
-      else:
-        ledstatus.on()
-        print("no")
-        bs.wait_for_press(timeout=None)
+          set_sumled(404) #reset the board
+          #Fast blinking to indicate reset
+          ledstatus.blink(on_time=0.2,off_time=0.2,n=20,background=False)
+          error = False
+          if error_count > 10:
+            #Turn off the Script for more than 5 errors
+            #Then debugging must be done
+            running = False
     except GPIOZeroError:
       #Set a blink led
+      #If the PI Errors Out
+      #can be resetet by
+      error_count += 1
+      error=True
       ledstatus.blink()
-      #running=False
 
 #Execution
 if __name__=="__main__":
