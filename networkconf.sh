2a6089d495e470a82db8be15bdebe5c6cc8b89d2
Dominic Hößl <dominic.hoessl@noris.de>
2019-04-12 14:54:58 +0200

    copy must be done with root rights

--- networkconf.sh
+++ networkconf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp /etc/network/interfaces /etc/network/interfaces-bak
-cp /home/pi/Tabletennis_Score_Display/interfaces /etc/network/interfaces
+sudo cp /etc/network/interfaces /etc/network/interfaces-bak
+sudo cp /home/pi/Tabletennis_Score_Display/interfaces /etc/network/interfaces
